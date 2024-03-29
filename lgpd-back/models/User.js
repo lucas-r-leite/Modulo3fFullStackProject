7fc8153c531f6f2619655665ad12658c2712f012
lucas <lucasgit.breeding934@passinbox.com>
2024-03-29 15:45:39 -0300

    User'image added to the backend

--- lgpd-back/models/User.js
+++ lgpd-back/models/User.js
@@ -1,23 +1,24 @@
-import {DataTypes } from "sequelize";
+import { DataTypes } from "sequelize";
 import sequelize from "../utils/database";
 
-const User = sequelize.define('users',{
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    first_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    last_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: DataTypes.STRING,
-    gender: DataTypes.STRING
-},{underscored:true})
+const User = sequelize.define('users', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: DataTypes.STRING,
+  gender: DataTypes.STRING,
+  profile_picture: DataTypes.STRING
+}, { underscored: true })
 
 export default User;
--- lgpd-back/routes/UserController.js
+++ lgpd-back/routes/UserController.js
@@ -1,45 +1,62 @@
 import express from "express";
+import multer from "multer";
+import process from "process";
+
 let router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, './images');
+  },
+  filename: function(req, file, callback) {
+    callback(null, req.body.first_name + "_" + req.body.last_name + "_" + Date.now() + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage }).single('file');
 
 import UserService from "../services/UserService.js"
 
-router.post("/addUser", async function(req,res){
-    const userModel = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        gender: req.body.gender
-    }
+router.post("/addUser", async function(req, res) {
+  const userModel = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    gender: req.body.gender,
+    profile_picture: req.file.path
+  }
 
-    const user = await UserService.saveUser(userModel);
-    return res.status(200).json(user);
+  const user = await UserService.saveUser(userModel);
+  return res.status(200).json(user);
 });
 
-router.get("/getAllUsers", async function(req,res){
-    const allUsers = await UserService.getAllUsers();
-    return res.status(200).json(allUsers);
+router.get("/getAllUsers", async function(req, res) {
+  const allUsers = await UserService.getAllUsers();
+  return res.status(200).json(allUsers);
 });
 
-router.get("/user/:id", async function(req,res){
-    const user = await UserService.getUserById(req.params.id);
-    return res.status(200).json(user);
+router.get("/user/:id", async function(req, res) {
+  const user = await UserService.getUserById(req.params.id);
+  return res.status(200).json(user);
 });
 
-router.delete("/deleteUser/:id", async function(req,res){
-    const user = await UserService.deleteUserById(req.params.id);
-    return res.status(200).json(user);
+router.delete("/deleteUser/:id", async function(req, res) {
+  const user = await UserService.deleteUserById(req.params.id);
+  return res.status(200).json(user);
 });
 
-router.put("/updateUser/:id", async function(req,res){
-    const userModel = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        gender: req.body.gender
-    }
+router.put("/updateUser/:id", async function(req, res) {
+  const userModel = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    gender: req.body.gender,
+    profile_picture: req.file.path
 
-    const user = await UserService.updateUserById(userModel);
-    return res.status(200).json(user);
+  }
+
+  const user = await UserService.updateUserById(userModel);
+  return res.status(200).json(user);
 });
 
 export default router;
